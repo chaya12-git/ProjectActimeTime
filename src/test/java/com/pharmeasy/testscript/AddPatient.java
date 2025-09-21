b141ff2a1de864bd086a91c2146c018fe9e42ec0
Dell <Dell@DESKTOP-CCC9FN8>
2025-09-21 14:26:54 +0530

    commit test

---
+++ src/test/java/com/pharmeasy/testscript/AddPatient.java
@@ -0,0 +1,15 @@
+package com.pharmeasy.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddPatient {
+	@Test
+	
+
+public void addNewPatient() {
+Reporter.log("pass",true);
+
+
+}
+}
